99b864e7134d3a032b7b75d72618c6484e42a248
rohitidke <rtrohittidke3@gmail.com>
2020-07-20 01:44:51 +0530

    home endpoint changed

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def create_app(test_config=None):
                              'GET, POST, PATCH, DELETE, OPTIONS')
         return response
 
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         '''
         Test Api.
